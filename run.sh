e2965acfe7f6e4eb9cd34c8bbd5a904f60923ca7
Zach Mitchell <zmitchell@fastmail.com>
2017-01-16 10:57:03 -0500

    Troubleshooting - HA doesn’t start

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-docker run -d --name hass --net=host -v /etc/localtime:/etc/localtime:ro -v /home/pi/hass/config:/config zmitchell/rpi-home-assistant:latest
+docker run --name hass --net=host -v /etc/localtime:/etc/localtime:ro -v /home/pi/hass/config:/config zmitchell/rpi-home-assistant:latest
