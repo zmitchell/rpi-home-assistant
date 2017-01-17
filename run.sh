79c445dcc116b12f98c1793ffbc41ac0147e5466
Zach Mitchell <zmitchell@fastmail.com>
2017-01-16 19:26:31 -0500

    Added the z-wave stick device to the container

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-docker run --name hass --net=host -v /etc/localtime:/etc/localtime:ro -v /home/pi/hass/config:/config zmitchell/rpi-home-assistant:0.35.3
+docker run --name hass --net=host -v /etc/localtime:/etc/localtime:ro -v /home/pi/hass/config:/config --device /dev/ttyACM0:/dev/zwave --rm zmitchell/rpi-home-assistant:0.35.3
