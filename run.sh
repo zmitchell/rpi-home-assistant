4dd0511272dc3596021b1295832928346dbb206f
Zach Mitchell <zmitchell@fastmail.com>
2017-01-31 19:49:44 +0000

    Added a section in run.sh for running detached

--- run.sh
+++ run.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
-CONFIG=/home/pi/hass/config
+CONFIG=/home/pi/homeassistant-config
 ZW_STICK=/dev/ttyACM0
 HA_VERSION=0.35.3 # alternatively, use "latest"
 IMAGE=zmitchell/rpi-home-assistant:$HA_VERSION
 
-docker run --name hass \
+# Use the first line instead to show HA log output and remove the
+# container once it exits.
+
+docker run --rm --name hass \
     --net=host \
     -v /etc/localtime:/etc/localtime:ro \
     -v $CONFIG:/srv/hass/config \
-    --device $ZW_STICK:/dev/zwave --rm \
+    --device $ZW_STICK:/dev/zwave \
     $IMAGE
+#docker run -d --name hass \
+#    --net=host \
+#    -v /etc/localtime:/etc/localtime:ro \
+#    -v $CONFIG:/srv/hass/config \
+#    --device $ZW_STICK:/dev/zwave \
+#    $IMAGE
